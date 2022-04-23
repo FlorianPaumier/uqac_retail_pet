ecfb5951022770ea494a544c003ad8a8db92e304
mawlaa-tech <mawlaa29drame@gmail.com>
2022-04-23 15:30:23 -0400

    le card pour afficher pour afficher les annonces sur accueil dans un recycleview'

---
+++ app/src/main/java/com/uqac/pet_retail/annonceList.kt
@@ -0,0 +1,11 @@
+package com.uqac.pet_retail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class annonceList : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_annonce_list)
+    }
+}
